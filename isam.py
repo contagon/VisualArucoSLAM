e0f671fb32e42989bb3195543e8e5d6536bd030d
Easton Potokar <contagon6@gmail.com>
2021-04-01 15:35:31 -0600

    Did a test run with 1 camera, seems to be working

--- isam.py
+++ isam.py
@@ -1,71 +1,93 @@
 import gtsam
 from gtsam.symbol_shorthand import L, X
+import numpy as np
+import cv2
+from plotting import *
 
-def plot_3d(estimate):
-    pass
-
-def plot(estimate):
-    pass
+np.set_printoptions(suppress=True)
 
 i = 0
-num_l = 10
 seen = set()
 
-# TODO set up camera and import intrinsic parameters
+# set up camera and import intrinsic parameters
+params = np.load('params/left.npz')
+mtx = params['mtx']
+dist = params['dist']
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_APRILTAG_36h11)
+aruco_params = cv2.aruco.DetectorParameters_create()
+cap = cv2.VideoCapture(2)
+size = 2.65 #inches
 
 # setup iSAM
 parameters = gtsam.ISAM2Params()
 parameters.setRelinearizeThreshold(0.01)
 parameters.setRelinearizeSkip(1)
 isam = gtsam.ISAM2(parameters)
-model = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.1, 0.3, 0.3, 0.3]))
+model = gtsam.noiseModel.Diagonal.Sigmas(np.array([.1, .1, .1, 5, 5, 5]))
 
 # Create a Factor Graph and Values to hold the new data
 graph = gtsam.NonlinearFactorGraph()
 initial_estimate = gtsam.Values()
+
+# setup plot
+fig = plt.figure(figsize=plt.figaspect(1/2))
+ax_2d = fig.add_subplot(1, 2, 1)
+ax_3d = fig.add_subplot(1, 2, 2, projection='3d')
+plt.ion()
 
 while(True):
     #obtain camera image
     ret0, img = cap.read()                                                                                                                          
 
     #detect the markers in the image, and add them to the graph
-    markerCorners, markerIds, rejecteCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters=parameters)
+    markerCorners, markerIds, rejecteCandidates = cv2.aruco.detectMarkers(img, dictionary, parameters=aruco_params)
     img = cv2.aruco.drawDetectedMarkers(img, markerCorners, markerIds)
 
     if markerIds is not None:
         for corners, id in zip(markerCorners, markerIds.flatten()):
             # estimate pose for each marker
             R, T, _ = cv2.aruco.estimatePoseSingleMarkers(corners, size, mtx, dist)
+            R, _ = cv2.Rodrigues(R)
+            T = T.squeeze()
 
             # add factor to graph
-            l_pose = gtsam.Pose3(R, T)
-            # TODO Verify I've got X and L in the right order here
+            l_pose = gtsam.Pose3(gtsam.Rot3(R), T)
             graph.add(gtsam.BetweenFactorPose3(X(i), L(id), l_pose, model))
 
             # add an estimate if we haven't seen it before
             if id not in seen:
                 seen.add(id)
-                # TODO Come up with better inital estimate for landmarks
                 initial_estimate.insert(L(id), gtsam.Pose3()) 
 
 
     if i == 0:
         # add in origin for first pose estimate
         initial_estimate.insert(X(0), gtsam.Pose3())
+        initial_estimate.insert(X(1), gtsam.Pose3())
 
         # Add a prior on pose x0
-        pose_noise = gtsam.noiseModel.Diagonal.Sigmas(np.array(
-            [0.1, 0.1, 0.1, 0.3, 0.3, 0.3]))  # 30cm std on x,y,z 0.1 rad on roll,pitch,yaw
-        graph.push_back(gtsam.PriorFactorPose3(X(0), gtsam.Pose3(), pose_noise))
+        graph.push_back(gtsam.PriorFactorPose3(X(0), gtsam.Pose3(), model))
 
     else:
-        # Guess new location is close to old one :)
-        initial_estimate.insert(X(i), estimate.atPose3(X(i-1)))
+        # Guess new location if close to old one :)
+        if i != 1:
+            initial_estimate.insert(X(i), estimate.atPose3(X(i-1)))
 
         # update isam
         isam.update(graph, initial_estimate)
         estimate = isam.calculateEstimate()
+        
+        # plot
+        plot_3d(estimate, ax_3d, seen)
+        plot_2d(estimate, isam, ax_2d, seen)
+        plt.pause(.00001)
 
         # clear everything out for next run
         graph.resize(0)
         initial_estimate.clear()
+
+    i += 1
+    cv2.imshow('image', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+    # print(i)
---
+++ plotting.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # pylint: disable=W0611
+import gtsam.utils.plot as gtsam_plot
+from matplotlib import patches
+from gtsam.symbol_shorthand import L, X
+
+def set_axes_equal(ax):
+    """
+    Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+    Args:
+      fignum (int): An integer representing the figure number for Matplotlib.
+    """
+    limits = np.array([
+        ax.get_xlim3d(),
+        ax.get_ylim3d(),
+        ax.get_zlim3d(),
+    ])
+
+    origin = np.mean(limits, axis=1)
+    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+
+    ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
+    ax.set_ylim3d([origin[1] - radius, origin[1] + radius])
+    ax.set_zlim3d([origin[2] - radius, origin[2] + radius])
+
+def plot_3d(result, ax, seen):
+    ax.cla()
+
+    # Plot cameras
+    i = 0
+    while result.exists(X(i)):
+        pose_i = result.atPose3(X(i))
+        gtsam_plot.plot_pose3_on_axes(ax, pose_i, 10)
+        i += 1
+
+    # plot landmarks
+    for i in seen:
+        pose_i = result.atPose3(L(i))
+        gtsam_plot.plot_pose3_on_axes(ax, pose_i, 5)
+
+    # draw
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    set_axes_equal(ax)
+    ax.view_init(-55, -85)
+
+def plot_2d(result, isam, ax, seen):
+    idx = np.ix_((0,-1), (0,-1))
+    ax.cla()
+
+    # plot cameras
+    i = 0
+    cameras = []
+    while result.exists(X(i)):
+        # get all data from pose
+        pose = result.atPose3(X(i))
+        # R = pose.rotation().matrix()
+        # cov = isam.marginalCovariance(X(i))[3:6,3:6]
+        # cov = ( R@cov@R.T )[idx]
+        t = [result.atPose3(X(i)).x(), result.atPose3(X(i)).z()]
+
+        # do we want pose covariances?
+        # ax.add_patch(cov_patch(t, cov, 'b'))
+        cameras.append(t)
+
+        i += 1
+
+    # plot landmarks
+    landmarks = []
+    for i in seen:
+        # get all data from pose
+        pose = result.atPose3(L(i))
+        R = pose.rotation().matrix()
+        cov = isam.marginalCovariance(L(i))[3:6,3:6]
+        cov = ( R@cov@R.T )[idx]
+        t = [result.atPose3(L(i)).x(), result.atPose3(L(i)).z()]
+
+        ax.add_patch(cov_patch(t, cov, 'r'))
+        landmarks.append(t)
+
+    cameras = np.array(cameras)
+    landmarks = np.array(landmarks)
+
+    ax.plot(cameras[:,0], cameras[:,1], label="Camera Poses", c='b', marker='o')
+    ax.scatter(landmarks[:,0], landmarks[:,1], label="Landmarks", c='r')
+
+    ax.legend()
+    ax.set_xlabel("X")
+    ax.set_ylabel("Z")
+    ax.set_aspect('equal')
+
+def cov_patch(origin, cov, color):
+    k = 5
+    w, v = np.linalg.eig(cov)
+    angle = np.arctan2(v[1, 0], v[0, 0])
+    return patches.Ellipse(origin, np.sqrt(w[0]*k), np.sqrt(w[1]*k),
+                             np.rad2deg(angle), fill=False, color=color, alpha=0.8)
